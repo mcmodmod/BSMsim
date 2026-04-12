3f2ec038c542d48d2ab29f411d76d8aca0f4fc58
mcmodmod <will.morten@yahoo.de>
2026-04-12 21:15:47 +0200

    Add Monte-Carlo simulation and tidy everything up a little

--- simulations.py
+++ simulations.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -15,12 +16,16 @@ class GeometricBrownianMotion:
     mu: float
     sigma: float
 
-    def simulate(self, T: float, N: int, seed: int | None = None) -> np.ndarray:
+    def simulate_gbm(
+        self, T: float, N: int, seed: int | None = None
+    ) -> tuple[np.ndarray, np.ndarray]:
         """
+        Simulates geometric Brownian motion path.
         Parameters:
-            T: total time horizon
+            T: total time
             N: number of time steps
             seed: optional seed for reproducibility
+        Returns tuple(times, path)
         """
         if seed is not None:
             np.random.seed(seed)
@@ -30,4 +35,46 @@ class GeometricBrownianMotion:
         W = np.random.normal(0, np.sqrt(dt), size=N).cumsum()
         W = np.insert(W, 0, 0.0)  # W(0) = 0
         S = self.S0 * np.exp((self.mu - 0.5 * self.sigma**2) * t + self.sigma * W)
-        return S
+        return t, S
+
+    def simulate_gbm_final(self, T: float, n_results: int = 1, seed: int | None = None):
+        """
+        Returns value of geom. Brownian motion after time T, without computing the full path.
+        Parameters:
+            T: total time
+            n_results: optional length of output array
+            seed: optional seed for reproducibility
+        """
+        if seed is not None:
+            np.random.seed(seed)
+
+        Z = np.random.normal(size=n_results)
+        S0 = self.S0 * np.exp(
+            (self.mu - 0.5 * self.sigma**2) * T + self.sigma * np.sqrt(T) * Z
+        )
+        return S0
+
+
+if __name__ == "__main__":
+    S0 = 120
+    mu = 0.0
+    gbm = GeometricBrownianMotion(S0, mu, 0.1)
+    times, S_path = gbm.simulate_gbm(1, 250)
+    plt.style.use("seaborn-v0_8-whitegrid")
+    fig, ax = plt.subplots(figsize=(8, 5))
+
+    ax.plot(times, S_path, color="k", label=r"Price Path $S(t)$")
+    ax.plot(
+        times,
+        S0 * np.exp(mu * times),
+        linestyle="--",
+        color="orange",
+        label=r"Drift Path $S_0 e^{\mu t}$",
+    )
+    ax.hlines(S0, 0, 1, linestyle="-.", color="grey", label=r"Initial Value $S_0$")
+    # d = max(S0 - np.min(S_path), np.max(S_path) - S0) * (1 + 0.05)
+    # ax.set_ylim(S0 - d, S0 + d)
+    ax.set_xlabel("Time (Years)")
+    ax.set_ylabel("Asset Price")
+    ax.legend(fontsize=12)
+    plt.show()
--- streamlit_application.py
+++ streamlit_application.py
@@ -4,6 +4,7 @@ import numpy as np
 from simulations import GeometricBrownianMotion
 from blackscholes import BlackScholesMerton
 
+plt.style.use("seaborn-v0_8-whitegrid")
 plt.rcParams.update(
     {
         "text.usetex": True,
@@ -11,22 +12,14 @@ plt.rcParams.update(
 )
 
 
-def simulate_price_path(N, S0, mu, sigma, T=1.0):
-    # Simulate One GBM Path
-    gbm = GeometricBrownianMotion(S0, mu, sigma)
-    times = np.linspace(0, T, N + 1)
-    S_path = gbm.simulate(T, N)
-    ST = S_path[-1]
-    return times, S_path, float(ST)
-
-
 def main():
     # --- Streamlit App ---
     st.set_page_config(page_title="GBM & BSM Simulator", layout="centered")
-    st.title("Geometric Brownian Motion")
+    st.title("Black-Scholes-Merton Simulator")
+    st.subheader("Geometric Brownian Motion")
 
     st.write(
-        r"In the following, a price path is simulated from some initial price $S_0\equiv S(t=0)$ using geometric Brownian motion"
+        r"In the following, a price path of an asset is simulated for one year from some initial price $S_0\equiv S(t=0)$ using geometric Brownian motion"
     )
     st.latex(
         r"S(t) = S_0\exp\left( \left(\mu - \frac{\sigma^2}{2} \right)t + \sigma W(t)\right),"
@@ -39,21 +32,21 @@ def main():
 
     left, right = st.columns(2)
     S0 = left.number_input(
-        r"Initial Stock Price $S_0$", value=120.0, min_value=0.01, step=1.0
+        r"Initial Asset Price $S_0$", value=120.0, min_value=0.01, step=1.0
     )
-    N = right.number_input(r"Number of Steps $N$", value=250, min_value=10, step=10)
-    mu = right.number_input(r"Drift $\mu$", value=0.05, step=0.01, format="%.3f")
+    N = right.number_input(r"Number of Steps $N$", value=500, min_value=10, step=10)
+    mu = right.number_input(r"Drift $\mu$", value=0.0, step=0.01, format="%.3f")
     sigma = left.number_input(
         r"Volatility $\sigma$", value=0.1, min_value=0.005, step=0.005, format="%.3f"
     )
+    T = 1.0
+    gbm = GeometricBrownianMotion(S0, mu, sigma)
 
-    # Compute Black-Scholes Call Price
-    times, S_path, ST = simulate_price_path(N, S0, mu, sigma)
+    # Generate Price Path
+    times, S_path = gbm.simulate_gbm(T, N)
     if st.button("Regenerate"):
-        times, S_path, ST = simulate_price_path(N, S0, mu, sigma)
-
+        times, S_path = gbm.simulate_gbm(T, N)
     # Display Plot
-    plt.style.use("seaborn-v0_8-whitegrid")
     fig, ax = plt.subplots(figsize=(8, 5))
 
     ax.plot(times, S_path, color="k", label=r"Price Path $S(t)$")
@@ -61,26 +54,29 @@ def main():
         times,
         S0 * np.exp(mu * times),
         linestyle="--",
-        color="orange",
-        label=r"Drift $S_0 e^{\mu t}$",
+        color="blue",
+        label=r"Drift Path $S_0 e^{\mu t}$",
     )
-    ax.hlines(S0, 0, 1, linestyle="-.", color="grey", label=r"Initial Value $S_0$")
-    d = max(S0 - np.min(S_path), np.max(S_path) - S0) * (1 + 0.05)
-    ax.set_ylim(S0 - d, S0 + d)
+    ax.hlines(
+        S0, 0, 1, linestyle="-.", color="grey", alpha=0.8, label=r"Initial Value $S_0$"
+    )
+    # d = max(S0 - np.min(S_path), np.max(S_path) - S0) * (1 + 0.05)
+    # ax.set_ylim(S0 - d, S0 + d)
     ax.set_xlabel("Time (Years)")
-    ax.set_ylabel("Stock Price")
+    ax.set_ylabel("Asset Price")
     ax.legend(fontsize=12)
     st.pyplot(fig)
 
-    # -----------------------------------------------------
+    # -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
 
-    st.title("Black-Scholes-Merton Simulator")
+    st.subheader("Black-Scholes-Merton Pricing")
     st.write(r"""
         Next, a European Put or Call option with the above quantity as its underlying asset is constructed using the user inputs below together with the volatility $\sigma$ from above.
         Its appropriate price is calculated using the Black-Scholes-Merton (BSM) equation
         """)
     st.latex(r"""
-    V(\omega; T, K, \sigma, r) = \omega\left(S_0 \Phi_{0,1}(\omega d_1) - S_0 e^{-r T} \Phi_{0,1}(\omega d_2)\right)
+    V(\omega; T, K, \sigma, r) = \omega\left(S_0 \Phi_{0,1}(\omega d_1) - K e^{-r T} \Phi_{0,1}(\omega d_2)\right)
     """)
     st.write("with")
     st.latex(r"""
@@ -91,38 +87,38 @@ def main():
         The parameter $\omega=\pm 1$ serves to distinguish Put ($\omega=-1$) from Call ($\omega=+1$) options. 
         This way the two BSM equation can be combined into one.
         """)
-    options = ["Put", "Call"]
-    selection = st.pills("Option Type", options, default="Put")
+    options = ["European Call", "European Put"]
+    option_type = st.selectbox("Option Type", options)
+    is_call = option_type == "European Call"
     T = st.number_input(
         r"Time to Maturity $T$ (years)", value=1.0, min_value=0.01, step=0.1
     )
     K = st.number_input(r"Strike Price $K$", value=120.0, min_value=0.01, step=1.0)
     r = st.number_input(r"Risk-free Rate $r$", value=0.02, step=0.01, format="%.4f")
-    times, S_path, ST = simulate_price_path(N, S0, mu, sigma, T)
+    times, S_path = gbm.simulate_gbm(T, N)
+    ST = float(S_path[-1])
     bsm = BlackScholesMerton(S0, K, T, r, sigma)
-    if selection == "Put":
-        price = bsm.price("put")
-        cash_flow = max(K - ST, 0)
-        profit = cash_flow - price
-    elif selection == "Call":
-        price = bsm.price("call")
+    if is_call:
         cash_flow = max(ST - K, 0)
-        profit = cash_flow - price
     else:
-        st.write("Please select either 'Put' or 'Call' above. Falling back to 'Call'.")
-        price = bsm.price("call")
-        cash_flow = max(ST - K, 0)
-        profit = cash_flow - price
+        cash_flow = max(K - ST, 0)
+    price = bsm.price("call") if is_call else bsm.price("put")
+    discounted_profit = np.exp(-r * T) * cash_flow - price
     st.write(f"The appropriate price for this option is **{price:.2f}**")
-    # if st.button("Generate New Price Path"):
-    #     times, S_path, ST = simulate_price_path(N, S0, mu, sigma)
-    st.write(f"Profit from this simulation: **{profit:.2f}**")
+    if st.button("Generate New Price Path"):
+        times, S_path = gbm.simulate_gbm(T, N)
+        ST = float(S_path[-1])
+        if is_call:
+            cash_flow = max(ST - K, 0)
+        else:
+            cash_flow = max(K - ST, 0)
+        price = bsm.price("call") if is_call else bsm.price("put")
+        discounted_profit = np.exp(-r * T) * cash_flow - price
+    st.write(f"Discounted profit from this simulation: **{discounted_profit:.2f}**")
 
     # Display Plot
-    plt.style.use("seaborn-v0_8-whitegrid")
     fig, ax = plt.subplots(figsize=(8, 5))
-    # Profit coloring
-    if profit > 0:
+    if discounted_profit > 0:
         ax.vlines(T, K + price, ST, color="limegreen", linewidth=2, label="Profit")
     else:
         ax.vlines(T, ST, K + price, color="red", linewidth=2, label="Loss")
@@ -131,23 +127,67 @@ def main():
     # d = max(S0 - np.min(S_path), np.max(S_path) - S0) * (1 + 0.05)
     # ax.set_ylim(S0 - d, S0 + d)
     ax.hlines(K, 0, T, linestyle="--", color="grey", label=r"Strike Price $K$")
-    ax.hlines(K + price, 0, T, linestyle="--", color="orange", label="Strike + Premium")
+    ax.hlines(K + price, 0, T, linestyle="--", color="blue", label="Strike + BSM Price")
     ax.set_xlabel("Time (Years)")
-    ax.set_ylabel("Stock Price")
-    ax.legend(fontsize=12)
+    ax.set_ylabel("Asset Price")
+    ax.legend(fontsize=10)
     st.pyplot(fig)
-    # Display Profits
 
-    # Monte Carlo Average Profit
-    # if st.button("Run Monte Carlo (1000 simulations)"):
-    #     profits = []
-    #     for _ in range(1000):
-    #         S_sim = gbm.simulate(T, N)
-    #         ST_sim = S_sim[-1]
-    #         cash_flow_sim = max(ST_sim - K, 0)
-    #         profits.append(cash_flow_sim - price)
-    #     avg_profit = np.mean(profits)
-    #     st.write(f"Average profit over 1000 simulations: **{avg_profit:.2f}**")
+    # -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+
+    st.subheader("Small Monte-Carlo Simulation")
+    no_of_simulations = st.number_input(
+        r"Number of Simulations", value=100_000, min_value=100, step=100
+    )
+    st.write(
+        f"Press the button below to simulate buying this option {no_of_simulations} times at the BSM-optimal price to approximate the average profit."
+    )
+    st.write(
+        r"""**Important note**: From now on the underlying asset is assumed to have a drift equal to the risk-free rate: $\mu=r$ (risk-neutral pricing).
+        This way, the average discounted profit should be zero.
+        """
+    )
+    mu = r
+    # if st.button("Run Monte-Carlo"):
+    gbm = GeometricBrownianMotion(S0, mu, sigma)
+    STs = gbm.simulate_gbm_final(T, no_of_simulations)
+    if is_call:
+        cash_flows = np.maximum(STs - K, 0)
+    else:
+        cash_flows = np.maximum(K - STs, 0)
+    discounted_profits = np.exp(-r * T) * cash_flows - price
+    avg_profit = np.mean(discounted_profits)
+    st.write(
+        f"Average discounted profit over {no_of_simulations} simulations: **{avg_profit:.3f}**"
+    )
+
+    # Convergence
+    st.write(
+        "The convergence to zero can be checked by plotting the absolute deviation from 0 as below."
+    )
+    fig, ax = plt.subplots()
+    cummean = np.cumsum(discounted_profits) / np.arange(1, no_of_simulations + 1)
+    simulations = np.arange(1, no_of_simulations + 1)
+    ax.plot(
+        simulations, np.abs(cummean), color="k", label="Absolute value of mean profit"
+    )
+    ax.plot(
+        simulations,
+        1 / np.sqrt(simulations),
+        linestyle="dashdot",
+        color="blue",
+        label=r"Expected Convergence $1/\sqrt{N}$",
+    )
+    ax.set_xlabel("Number of Simulations N")
+    ax.set_ylabel("Error")
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    ax.legend()
+    st.pyplot(fig)
+    st.write(
+        "As expected, the mean converges with the inverse square-root of the number of simulations."
+    )
 
 
 if __name__ == "__main__":
